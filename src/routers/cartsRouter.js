4f8511ad05020d45dccd2eafc2e22ad7826a2619
Fabrizio Fontanarrosa <fabriziofontanarrosa@gmail.com>
2026-02-01 22:04:48 -0300

    fix synced execution of cart saving

--- src/routers/cartsRouter.js
+++ src/routers/cartsRouter.js
@@ -5,17 +5,15 @@ export default function cartsRouter(cartManager) {
 
 	//    POST /carts/
 	/*    Debe crear un nuevo carrito con la siguiente estructura:
-
             id: Number/String (Autogenerado para asegurar que nunca se dupliquen los ids).
-
             products: Array que contendrá objetos que representen cada producto.
 */
 
-	router.post("/", (req, res) => {
+	router.post("/", async (req, res) => {
 		console.log(`Petición POST /carts/ recibida`);
 		cartManager.addCart();
+		await cartManager.save("./carts.json");
 		res.send();
-		cartManager.save("./carts.json");
 	});
 
 	//    GET /carts/:cid
@@ -29,22 +27,20 @@ export default function cartsRouter(cartManager) {
 
 	//    POST /carts/:cid/product/:pid
 	/*    Debe agregar el producto al arreglo products del carrito seleccionado, utilizando el siguiente formato:
-
             product: Solo debe contener el ID del producto.
-
             quantity: Debe contener el número de ejemplares de dicho producto (se agregará de uno en uno).
 
     Si un producto ya existente intenta agregarse, se debe incrementar el campo quantity de dicho producto.
 
 */
-	router.post("/:cid/product/:pid", (req, res) => {
+	router.post("/:cid/product/:pid", async (req, res) => {
 		try {
 			const cid = req.params.cid;
 			const pid = req.params.pid;
 			console.log(`Petición GET /carts/${cid}/product/${pid} recibida`);
 			cartManager.addProductToCart(cid, pid, productManager);
+			await cartManager.save("./carts.json");
 			res.send();
-			cartManager.save("./carts.json");
 		} catch (error) {
 			console.error(error);
 		}
